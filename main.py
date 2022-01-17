153eee33991deb1e0fb44b0a3282e298ec3d1c4d
IamOppy <randolffretrato@yahoo.com>
2022-01-16 18:49:10 -0800

    Changes to button sizes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from fileshare import FileSharer
+from kivy.core.clipboard import Clipboard
 import time
+import webbrowser
 
 Builder.load_file('frontend.kv')
 
@@ -23,11 +25,28 @@ class CameraScreen(Screen):
         filePath = f"capturedFiles/{current_time}.png"
         self.ids.camera.export_to_png(filePath)
         self.manager.current = "image_screen"
+        self.manager.current_screen.ids.img.source = filePath
 
 class ImageScreen(Screen):
-    pass
+    link_message = 'Create a Link First'
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        filesharer = FileSharer(filepath=file_path)
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
 
-
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+            
+    def open_link(self):
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
+        
 
 class RootWidget(ScreenManager):
     pass
